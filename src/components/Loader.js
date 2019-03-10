b75dc09b5ad98e95b66092f9d36f6a691a81515f
Riku Mäntysalo <rikumantysalo@Rikus-MacBook-Pro.local>
2019-03-10 14:46:31 +0200

    fix: remove unused props input

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CircularProgress } from '@material-ui/core'
 
-export default function CircularIndeterminate(props) {
+export default function CircularIndeterminate() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <CircularProgress disableShrink />
